92586243649c2ab66116b3efdee89cd818931206
kang <stellaalsrb@gmail.com>
2022-08-08 16:05:10 +0900

    [Add] emotion

---
+++ src/api/musicData.ts
@@ -0,0 +1,6 @@
+
+
+export const getSongsApi = (value:any) =>{
+    
+
+}
---
+++ src/style/global.style.ts
@@ -0,0 +1,13 @@
+import { css } from '@emotion/react'
+
+
+const titleStyle = css({
+  boxSizing: 'border-box',
+  width: 300,
+  height: 200,
+})
+
+let style = css`
+  color: hotpink;
+  label: some-name;
+`
